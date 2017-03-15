d6e9391dab30f4fa849370bcf7405de4cad10909
Clint <clintvencor@gmail.com>
2017-03-15 09:21:50 +0000

    finish initial layouts

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,24 @@
 class PagesController < ApplicationController
+    def landing
+        @content = Content.find(1)
+        @client = Client.new
+    end
+    
+    def admin
+        
+    end
+    
+    def create
+        @client = Client.create(client_params)
+        if @client.save
+            redirect_to admin_path
+        else
+            render "landing"
+        end
+    end
+    
+    private
+        def client_params
+            params.require(:client).permit(:name, :email, :contact_number)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+    post "/client" => "pages#create", as: "send"
+    get "/admin" => "pages#admin"
     root "pages#landing"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
